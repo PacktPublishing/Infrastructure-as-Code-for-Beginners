3bd268d315186e819c4795f5c71931043ccd2f3f
Russ McKendrick <russ@mckendrick.io>
2023-01-28 16:22:21 +0000

    Update -

--- Chapter05/terraform-aws/003-storage.tf
+++ Chapter05/terraform-aws/003-storage.tf
@@ -1,17 +1,17 @@
-# # Create EFS File System
+# Create EFS File System
 resource "aws_efs_file_system" "efs" {
   creation_token = "${var.name}-${var.environment_type}-nfs"
   tags           = merge(var.default_tags, tomap({ Name = "${var.name}-${var.environment_type}-efs" }))
 }
 
-# # Add a mount target for each subnet
+# Add a mount target for web01 subnet
 resource "aws_efs_mount_target" "efs_mount_targets01" {
   file_system_id  = aws_efs_file_system.efs.id
   subnet_id       = aws_subnet.web01.id
   security_groups = [aws_security_group.sg_efs.id]
 }
 
-# # Add a mount target for each subnet
+# Add a mount target for web02 subnet
 resource "aws_efs_mount_target" "efs_mount_targets02" {
   file_system_id  = aws_efs_file_system.efs.id
   subnet_id       = aws_subnet.web02.id
